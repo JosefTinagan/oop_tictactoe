66c510c23079c39065640c791020cec38f84cb0a
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-11-04 17:08:03 +0800

    working prototype

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -2,9 +2,10 @@ class TicTacToe
 	attr_accessor :name
 	attr_reader :sign
 
+	temp_period = "."
 	@@turn = 0
 	@@players = 0
-	@@arr_board = Array.new(9)
+	@@game_board = Array.new(9,temp_period)
 	@@arr_temp = []
 
 	def initialize(name="You")
@@ -27,8 +28,8 @@ class TicTacToe
 	end
 
 	def turn
-		check_victory?() 
-		if @@turn == 9
+		
+		if @@turn ==  9
 			game_over()
 		end
 		puts "#{@name} is playing"
@@ -46,11 +47,12 @@ class TicTacToe
 		puts "Updated Board:"
 		TicTacToe.game_board
 		puts
-		
+		check_victory?() 
 		@@turn += 1
 	end
 
 	def change_board(choice)
+		#check if spot is taken
 		taken = false
 		@@arr_temp.each do |x|
 			if x == choice
@@ -62,21 +64,23 @@ class TicTacToe
 			puts "This spot is already taken..."
 				puts "Try again..."
 				puts 
+				@@turn -= 1
 				turn()
 		else
 			case choice.upcase
-			when "TL" then @@arr_board[0] = "#{@sign}"
-			when "T"  then @@arr_board[1] = "#{@sign}"
-			when "TR" then @@arr_board[2] = "#{@sign}"
-			when "ML" then @@arr_board[3] = "#{@sign}"
-			when "M"  then @@arr_board[4] = "#{@sign}"
-			when "MR" then @@arr_board[5] = "#{@sign}"
-			when "BL" then @@arr_board[6] = "#{@sign}"
-			when "B"  then @@arr_board[7] = "#{@sign}"
-			when "BR" then @@arr_board[8] = "#{@sign}"
+			when "TL" then @@game_board[0] = "#{@sign}"
+			when "T"  then @@game_board[1] = "#{@sign}"
+			when "TR" then @@game_board[2] = "#{@sign}"
+			when "ML" then @@game_board[3] = "#{@sign}"
+			when "M"  then @@game_board[4] = "#{@sign}"
+			when "MR" then @@game_board[5] = "#{@sign}"
+			when "BL" then @@game_board[6] = "#{@sign}"
+			when "B"  then @@game_board[7] = "#{@sign}"
+			when "BR" then @@game_board[8] = "#{@sign}"
 			else 
 				puts 
 				puts "Not a valid choice... Try again"
+				@@turn -= 1
 				turn()
 			end
 
@@ -85,12 +89,101 @@ class TicTacToe
 		
 	end
 
-	def self.game_board
+	def check_victory?
+		number_of_x = 0
+		number_of_o = 0
 
-		@@arr_board.each_with_index do |x,y|
-			if x == nil
-				@@arr_board[y] = "."
+		#check top row
+		for x in 0..2
+			temp_a, temp_b = row_checking(x)
+			number_of_x += temp_a
+			number_of_o += temp_b
+		end
+		number_of_x, number_of_o = check_victory(number_of_x,number_of_o)
+		
+		#check middle row
+		for x in 3..5
+			temp_a, temp_b = row_checking(x)
+			number_of_x += temp_a
+			number_of_o += temp_b
+		end
+		number_of_x, number_of_o = check_victory(number_of_x,number_of_o)
+		
+		#check bottom row
+		for x in 6..8
+			temp_a, temp_b = row_checking(x)
+			number_of_x += temp_a
+			number_of_o += temp_b
+		end
+		number_of_x, number_of_o = check_victory(number_of_x,number_of_o)
+		
+
+		for x in 0..@@game_board.length - 1
+			#left column
+			if x == 0 || x == 3 || x == 6
+				z = @@game_board[x]
+				if z == "X" 
+					number_of_x += 1	
+				elsif z == "O"
+					number_of_o += 1
+				end
 			end
+		end
+		number_of_x, number_of_o = check_victory(number_of_x,number_of_o)
+		
+		for x in 0..@@game_board.length - 1
+			#middle column
+			if x == 1 || x == 4 || x == 7
+				z = @@game_board[x]
+				if z == "X" 
+					number_of_x += 1	
+				elsif z == "O"
+					number_of_o += 1
+				end	
+			end
+		end
+		number_of_x, number_of_o = check_victory(number_of_x,number_of_o)
+		
+		for x in 0..@@game_board.length - 1	
+			#right column
+			if x == 2 || x == 5 || x == 8
+				z = @@game_board[x]
+				if z == "X" 
+					number_of_x += 1	
+				elsif z == "O"
+					number_of_o += 1
+				end	
+			end
+		end
+		number_of_x, number_of_o = check_victory(number_of_x,number_of_o)
+		
+	end
+	
+	def row_checking(x)
+		y = 0
+		z = 0
+		if @@game_board[x] == "X"
+					y += 1
+		elsif @@game_board[x] == "O"
+					z += 1
+		end
+		
+		return y,z
+		
+	end
+
+	def check_victory(number_of_x, number_of_o)
+		if number_of_x == 3
+			victory
+		elsif number_of_o == 3
+			victory
+		else
+			return 0,0
+		end
+	end
+
+	def self.game_board
+		@@game_board.each_with_index do |x,y|
 			print "#{x} "
 				if y == 2 || y == 5
 					puts
@@ -99,32 +192,25 @@ class TicTacToe
 			
 	end
 
-	def number_of_players
-		@@players
-	end
-
-	def check_victory?
-		count_x = 0
-		count_o = 0
-		
-		for i in 0..2
-			
-			x = @@arr_board[i]
-			if x == "X"
-				count_x += 1
-			elsif x == "O"
-				count_o += 1
-			end
-			puts "X CHECKING: #{count_x}"
-			if count_x == 3
-				puts "Victory!"
-			end
+	def victory()
+		puts
+		puts "The #{@sign} sign is victorious!"
+		if @name == "You"
+			puts "#{@name} win!"
+		else
+			puts "#{@name} wins!"
 		end
+		exit
 	end
 
 	def game_over
-		puts "Game over"
+		puts
+		puts "Game over. No one wins!"
 		exit
+	end
+
+	def number_of_players
+		@@players
 	end
 end
 
